9aa89a14515a12b7c2a1414258a7807c590853f7
Martín <magadiflo@gmail.com>
2024-01-31 12:59:22 -0500

    Paquetes predefinidos

---
+++ 08.packages/07.paquetes_predefinidos.sql
@@ -0,0 +1,13 @@
+/***************************************************************************************************
+Paquetes predefinidos
+****************************************************************************************************
+- DBMS_OUTPUT, el que usamos para imprimir por pantalla.
+- DBMS_ALERT
+- DBMS_LOCK
+- DBMS_SESSION
+- DBMS_APPLICATION_INFO
+- DBMS_SCHEDULER
+- UTL_FILE, nos permite trabajar con ficheros externos a la base de datos.
+- UTL_MAIL
+- HTP
+*/
